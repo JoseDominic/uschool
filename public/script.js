0b10880996ab4cfe4e96d1be7f009dcdfbff47b5
josedominic <josudominic007@gmail.com>
2021-01-11 12:28:45 +0530

    screen share test

--- public/script.js
+++ public/script.js
@@ -3,16 +3,23 @@ console.log(socket);
 const roomIdView = document.getElementById('roomid')
 roomIdView.innerHTML = ROOM_ID
 const videoGrid = document.getElementById('video-grid')
+const shareScreenBtn = document.getElementById('shareScreen')
+
 const myPeer = new Peer(undefined, {
   host: '/',
   path:'peerjs',
-  port: '443' //use 443 in cloud deployment
+  port: '3000' //use 443 in cloud deployment
 })
-let myVideoStream;
+
+//let myVideoStream;
+let peerConnection;
+
 const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
-navigator.mediaDevices.getUserMedia({
+
+//fetch user video stream
+navigator.mediaDevices.getDisplayMedia({
   video: {width:640,height:360},
   audio: true
 }).then(stream => {
@@ -29,7 +36,7 @@ navigator.mediaDevices.getUserMedia({
   socket.on('user-connected', userId => {
     connectToNewUser(userId, stream)
   })
-  // input value
+  // input value for text chat
   let text = $("input");
   // when press enter send message
   $('html').keydown(function (e) {
@@ -66,6 +73,7 @@ function connectToNewUser(userId, stream) {
   })
 
   peers[userId] = call
+  peerConnection = call
 }
 
 function addVideoStream(video, stream) {
@@ -76,6 +84,36 @@ function addVideoStream(video, stream) {
   videoGrid.append(video)
 }
 
+//function to replace video stream
+function replaceStream(peerConnection, mediaStream) {
+  for(sender of peerConnection.getSenders()){
+      if(sender.track.kind == "audio") {
+          if(mediaStream.getAudioTracks().length > 0){
+              sender.replaceTrack(mediaStream.getAudioTracks()[0]);
+          }
+      }
+      if(sender.track.kind == "video") {
+          if(mediaStream.getVideoTracks().length > 0){
+              sender.replaceTrack(mediaStream.getVideoTracks()[0]);
+          }
+      }
+  }
+}
+
+//Screen Sharing
+shareScreenBtn.addEventListener('click', (event) => {
+    navigator.mediaDevices.getDisplayMedia({
+      video:{
+        height:480,
+        width:640
+      }
+    })
+    .then( (stream) => {
+      const video = document.createElement('video');
+      addVideoStream(video,stream);
+      //replaceStream(stream);
+    })
+})
 
 
 const scrollToBottom = () => {
