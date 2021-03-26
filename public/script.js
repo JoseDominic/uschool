a436ca38c9449ea5207c5911dd8b5e836255f271
josedominic <josudominic007@gmail.com>
2021-03-26 23:11:48 +0530

    update ss,con

--- public/script.js
+++ public/script.js
@@ -4,7 +4,6 @@ console.log(Navigator.onLine);
 const roomIdView = document.getElementById('roomid')
 roomIdView.innerHTML = ROOM_ID
 const videoGrid = document.getElementById('video-grid')
-const shareScreenBtn = document.getElementById('shareScreen')
 
 const myPeer = new Peer(undefined, {
   host: '/',
@@ -17,7 +16,7 @@ const myPeer = new Peer(undefined, {
 //   { url: 'turn:turn:numb.viagenie.ca:3478',username:'webrtc@live.com', credential: 'muazkh' }
 //   ]}
 
-//let myVideoStream;
+let myVideoStream;
 let peerConnection;
 
 const myVideo = document.createElement('video')
@@ -91,35 +90,21 @@ function addVideoStream(video, stream) {
 }
 
 //function to replace video stream
-function replaceStream(peerConnection, mediaStream) {
-  for(sender of peerConnection.getSenders()){
-      if(sender.track.kind == "audio") {
-          if(mediaStream.getAudioTracks().length > 0){
-              sender.replaceTrack(mediaStream.getAudioTracks()[0]);
-          }
-      }
-      if(sender.track.kind == "video") {
-          if(mediaStream.getVideoTracks().length > 0){
-              sender.replaceTrack(mediaStream.getVideoTracks()[0]);
-          }
-      }
-  }
-}
+// function replaceStream(peerConnection, mediaStream) {
+//   for(sender of peerConnection.getSenders()){
+//       if(sender.track.kind == "audio") {
+//           if(mediaStream.getAudioTracks().length > 0){
+//               sender.replaceTrack(mediaStream.getAudioTracks()[0]);
+//           }
+//       }
+//       if(sender.track.kind == "video") {
+//           if(mediaStream.getVideoTracks().length > 0){
+//               sender.replaceTrack(mediaStream.getVideoTracks()[0]);
+//           }
+//       }
+//   }
+// }
 
-//Screen Sharing
-shareScreenBtn.addEventListener('click', (event) => {
-    navigator.mediaDevices.getDisplayMedia({
-      video:{
-        height:480,
-        width:640
-      }
-    })
-    .then( (stream) => {
-      const video = document.createElement('video');
-      addVideoStream(video,stream);
-      //replaceStream(stream);
-    })
-})
 
 //internet connectivity monitoring
 //fetch fake data from api to check for internet
